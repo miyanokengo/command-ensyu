8463f1f840fc81470d0b010b40d92568f3bc7d61
miyano <k.miyano.sys22@morijyobi.ac.jp>
2022-11-08 10:50:29 +0900

    間違い変更完了　だとぉ

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         System.out.println("BMIを計算");
+        for(int i=0; i<2; i++){
         Scanner scan = new Scanner(System.in);
             System.out.print("・身長(m)を入力：");
                 double height =  scan.nextDouble();
@@ -9,5 +10,7 @@ public class Main{
                 double weight =  scan.nextDouble();
                 double bmi = weight / Math.pow(height, 2);
                 System.out.println("BMIは"+String.format("%.2f",bmi));
+	    }
+
     }
 }
